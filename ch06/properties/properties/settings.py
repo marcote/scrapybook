7af52395cc5d8c3a057016dbd5b7c985489876fd
Marcos Torres <marcost@lagash.com>
2016-07-06 11:30:34 -0300

    updating sample app to match my environment

--- ch06/properties/properties/settings.py
+++ ch06/properties/properties/settings.py
@@ -11,9 +11,17 @@ BOT_NAME = 'properties'
 SPIDER_MODULES = ['properties.spiders']
 NEWSPIDER_MODULE = 'properties.spiders'
 
+ITEM_PIPELINES = {
+    'scrapyelasticsearch.scrapyelasticsearch.ElasticSearchPipeline' : 1
+}
 # Crawl responsibly by identifying yourself (and your website) on
 # the user-agent
 #USER_AGENT = 'properties (+http://www.yourdomain.com)'
+
+ELASTICSEARCH_SERVERS = ['http://localhost:9200']
+ELASTICSEARCH_INDEX = 'scrapy'
+ELASTICSEARCH_TYPE = 'items'
+ELASTICSEARCH_UNIQ_KEY = 'url'
 
 # Disable S3
 AWS_ACCESS_KEY_ID = ""
